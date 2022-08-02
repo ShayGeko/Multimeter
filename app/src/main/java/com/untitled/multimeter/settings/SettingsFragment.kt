5c27d6419fe8b79b5d107771be12c45f2f5182af
Lucas <you@example.com>
2022-08-02 15:13:49 -0700

    Settings Fragment changed to PreferenceScreen Added Dark/Light mode

--- app/src/main/java/com/untitled/multimeter/settings/SettingsFragment.kt
+++ app/src/main/java/com/untitled/multimeter/settings/SettingsFragment.kt
@@ -1,65 +1,37 @@
 package com.untitled.multimeter.settings
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.*
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.preference.*
 import com.untitled.multimeter.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    private lateinit var sharedPref: SharedPreferences
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.fragment_settings, rootKey)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val view: View = inflater.inflate(R.layout.fragment_settings, container, false)
+        val username = findPreference<EditTextPreference>("username")
+        val name = findPreference<EditTextPreference>("name")
+        val themeSwitch: SwitchPreferenceCompat? = findPreference("theme mode")
+        val frequency = findPreference<ListPreference>("frequency")
+        val logoutBtn = findPreference<Preference>("logout")
 
-        sharedPref = requireActivity().applicationContext.getSharedPreferences("sharedPrefsKey", AppCompatActivity.MODE_PRIVATE)
-        val saveBtn = view.findViewById<Button>(R.id.saveBtn)
-
-        loadPreferences()
-
-        saveBtn.setOnClickListener {
-            savePreferences()
-            Toast.makeText(requireContext(), "Saved Settings", Toast.LENGTH_SHORT).show()
+        //set listener for theme switch
+        themeSwitch?.setOnPreferenceChangeListener { _,_ ->
+            checkTheme(themeSwitch.isChecked)
+            true
         }
-
-        return view
     }
 
-    private fun loadPreferences() {
-        view?.findViewById<EditText>(R.id.editTextUsername)?.setText(
-            sharedPref.getString("usernameKey", "").toString()
-        )
-        view?.findViewById<EditText>(R.id.editTextName)
-            ?.setText(sharedPref.getString("nameKey", "").toString())
-        view?.findViewById<Switch>(R.id.switchLightDarkMode)?.isChecked =
-            sharedPref.getBoolean("switchKey", false)
-        view?.findViewById<Spinner>(R.id.spinnerFrequency)?.setSelection(
-            sharedPref.getLong("frequencyKey", 0).toInt()
-        )
-    }
-
-    private fun savePreferences() {
-        val editor = sharedPref.edit()
-
-        val username: String = view?.findViewById<EditText>(R.id.editTextUsername)?.text.toString()
-        val name: String = view?.findViewById<EditText>(R.id.editTextName)?.text.toString()
-        val switch: Boolean = view?.findViewById<SwitchCompat>(R.id.switchLightDarkMode)!!.isChecked
-        val frequency: Long = (view?.findViewById<Spinner>(R.id.spinnerFrequency)?.selectedItemId ?:
-        editor.putString("usernameKey", username)) as Long
-        editor.putString("nameKey", name)
-        editor.putBoolean("switchKey", switch)
-        editor.putLong("frequencyKey", frequency)
-
-        editor.apply()
+    private fun checkTheme(input: Boolean) {
+        if (input) {
+            AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        }
     }
 }
